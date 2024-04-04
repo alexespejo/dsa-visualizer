bfd13154a506a2ab2cacf6f744942177025a95aa
Alex Espejo <aespejo102@gmail.com>
2024-04-04 00:16:18 -0700

    update readme

--- src/components/HashTableControls/Navbar.svelte
+++ src/components/HashTableControls/Navbar.svelte
@@ -6,12 +6,29 @@
 
 <div class="navbar bg-base-100 shadow-sm border-b-2">
  <div class="navbar-start">
-  <Link to="/">
-   <h1 class="text-xl sm:text-2xl lg:text-4xl font-bold">Hash Tables</h1>
+  <Link to="/" class="btn btn-ghost">
+   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    fill="currentColor"
+    class="bi bi-house"
+    viewBox="0 0 16 16"
+   >
+    <path
+     d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"
+    />
+   </svg>
   </Link>
+  <h1 class="text-xl sm:text-2xl lg:text-4xl font-bold lg:flex hidden">
+   Hash Tables
+  </h1>
  </div>
- <div class="ml-auto navbar-end">
-  <DropdownMenue label="More Hashtables"><HashTableRouteLinks /></DropdownMenue>
+ <div class="navbar-center lg:hidden">
+  <h1 class="text-xl sm:text-2xl lg:text-4xl font-bold">Hash Tables</h1>
+ </div>
+ <div class="navbar-end">
+  <DropdownMenue label="Hash Tables"><HashTableRouteLinks /></DropdownMenue>
  </div>
  <!-- <div class="hidden sm:flex navbar-end">
     <a href="https://github.com/alexespejo/dsa-visualizer" class="btn  ">
--- src/components/NavbarDropDown/DropdownMenue.svelte
+++ src/components/NavbarDropDown/DropdownMenue.svelte
@@ -3,7 +3,24 @@
 </script>
 
 <div class="xl:hidden dropdown dropdown-end">
- <div tabindex="-1" role="button" class="btn m-1">{label}</div>
+ <div class="tooltip tooltip-left" data-tip={`More ${label}`}>
+  <div tabindex="-1" role="button" class="btn m-1">
+   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+    class="bi bi-list"
+    viewBox="0 0 16 16"
+   >
+    <path
+     fill-rule="evenodd"
+     d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+    />
+   </svg>
+  </div>
+ </div>
+
  <div
   tabindex="-1"
   class=" dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
@@ -14,6 +31,6 @@
  </div>
 </div>
 
-<div class="hidden join xl:flex">
+<div class="hidden join lg:flex">
  <slot />
 </div>
