24ea2dc833db7cc872d570fcf3614236047e4712
Alex Espejo <aespejo102@gmail.com>
2024-04-25 21:56:45 -0700

    document functions

--- src/lib/hashTableFunctions/linearProbing.ts
+++ src/lib/hashTableFunctions/linearProbing.ts
@@ -4,15 +4,15 @@ function insertLinear(
  stepsize: number,
  capacity: number
 ) {
+ // Since the array is preset with undefined values, we can check if the array is full so use this instead to check if the array is full
  if (!table.includes(undefined)) {
   return table;
  }
- if (table[table.length - 1] === 20) {
-  return table;
- }
+
  let i: number = 0;
- let loopCycles: number = 0;
+ let loopCycles: number = 0; // a loop cycle is a full iteration of the array
  while (loopCycles < 5) {
+  // instead of a load factor check, we will limit the loop to 5 cycles
   if (i >= capacity) {
    loopCycles++;
   }
@@ -32,7 +32,7 @@ function removeLinear(table: any[], data: number) {
  if (!table.includes(data)) {
   return table;
  }
-
+ // They don't have to know this is how we implemented it lol
  table[table.indexOf(data)] = undefined;
  return table;
 }
