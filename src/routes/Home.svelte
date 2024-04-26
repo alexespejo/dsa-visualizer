6bdb9085a54a7398ab7af15eebcc09571f6a65df
Alex Espejo <aespejo102@gmail.com>
2024-04-25 17:01:01 -0700

    created queue card

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -102,4 +102,33 @@
    </div>
   </div>
  </div>
+ <div class="card w-full sm:w-72 h-96 bg-base-100 shadow-xl">
+  <figure>
+   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    class="bi bi-arrow-clockwise w-36 h-36"
+    viewBox="0 0 16 16"
+   >
+    <path
+     fill-rule="evenodd"
+     d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
+    />
+    <path
+     d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"
+    />
+   </svg>
+  </figure>
+  <div class="card-body">
+   <h2 class="card-title">Circular Queue</h2>
+   <p></p>
+   <div class="card-actions justify-end">
+    <button class="btn btn-warning"
+     >Coming Soon<span class="loading loading-dots loading-xs"></span></button
+    >
+   </div>
+  </div>
+ </div>
 </div>
