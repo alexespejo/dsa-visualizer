6ddb36425456f99461babb3c5f91db2d8e69e12e
Alex Espejo <aespejo102@gmail.com>
2024-04-28 02:09:10 -0700

    create custom components

---
+++ src/components/custom/Inputs/TextInput.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+ export let value: string;
+ export let color: string;
+ export let sz: string = "";
+ export let styles: string = "";
+ export let placeholder: string = "";
+</script>
+
+<input
+ bind:value
+ type="text"
+ placeholder={placeholder}
+ class={`input input-${color} input-${sz} ${styles} input-bordered w-max-w-xs font-bold`}
+/>
---
+++ src/routes/ComponentPlayground.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+ import TextInput from "../components/custom/Inputs/TextInput.svelte";
+ import FormControl from "../components/HashTableControls/FormControl.svelte";
+ import LabelInput from "../components/custom/Inputs/Label.svelte";
+
+ let value = "";
+</script>
+
+<div class="">
+ playground
+ <main class="">
+  <h1 class="">
+   {value}
+  </h1>
+  <FormControl>
+   <LabelInput>Type Here</LabelInput>
+   <TextInput bind:value color="primary" placeholder="Enter your name" />
+  </FormControl>
+ </main>
+</div>
