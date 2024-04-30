f77f69ca2a42abd560e8dd0c047109cad97885c4
Alex Espejo <aespejo102@gmail.com>
2024-04-30 12:35:11 -0700

    update sorting algo routes

--- src/components/SortingAlgoControls/SortingAlgoRouteLinks.svelte
+++ src/components/SortingAlgoControls/SortingAlgoRouteLinks.svelte
@@ -1,11 +1,11 @@
 <script>
-import {Link} from "svelte-routing"
+ import { Link } from "svelte-routing";
 </script>
 
-<Link class="btn join-item" to="/">Bubble</Link>
-<Link class="btn join-item" to="/">Selection</Link>
-<Link class="btn join-item" to="/">Insertion</Link>
+<Link class="btn join-item" to="/sorting_algo/bubble_sort">Bubble</Link>
+<Link class="btn join-item" to="/sorting_algo/selection_sort">Selection</Link>
+<Link class="btn join-item" to="/sorting_algo/insertion_sort">Insertion</Link>
 <Link class="btn join-item" to="/sorting_algo/bucket_sort">Bucket</Link>
 <Link class="btn join-item" to="/sorting_algo/radix_sort">Radix</Link>
-<Link class="btn join-item" to="/">Merge</Link>
-<Link class="btn join-item" to="/">Quicksort</Link>
+<Link class="btn join-item" to="/sorting_algo/merge_sort">Merge</Link>
+<Link class="btn join-item" to="/sorting_algo/quick_sort">Quicksort</Link>
