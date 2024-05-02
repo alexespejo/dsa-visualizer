e38f7d590b4c31d6fa452b37b5db28079bfdc380
Alex Espejo <aespejo102@gmail.com>
2024-05-02 14:10:40 -0700

    create recursive nodes

---
+++ src/components/Trees/RecursiveNode2.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+ interface RecursiveNode {
+  head: number;
+  left: RecursiveNode | null;
+  right: RecursiveNode | null;
+ }
+
+ import RecursiveNode1 from "./RecursiveNode1.svelte";
+ export let value2: number[] | null = null;
+ export let index: number;
+</script>
+
+<div>
+ {#if index < value2.length && value2[index] !== null}
+  <span class={`${value2[index] === -1 ? "opacity-0" : ""}`}
+   >{value2[index]}</span
+  >
+  <RecursiveNode1 value1={value2} index={2 * index + 1} />
+  <RecursiveNode1 value1={value2} index={2 * index + 2} />
+ {/if}
+</div>
---
+++ src/components/Trees/TreeNode.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+ export let value: number = -1;
+</script>
+
+<div class={`${value === -1 ? "opacity-0" : ""}`}>
+ <span>{value}</span>
+ <slot />
+</div>
