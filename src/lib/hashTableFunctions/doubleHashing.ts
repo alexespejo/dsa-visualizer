0cd6550096b2aff0c7b416960627ded08aabfd36
Panteaforoutan <foroutan.pantea.98@gmail.com>
2024-05-08 11:48:09 -0700

    updated remove function

--- src/lib/hashTableFunctions/doubleHashing.ts
+++ src/lib/hashTableFunctions/doubleHashing.ts
@@ -1,53 +1,41 @@
 function insertDoubleHashing(
- table: number[],
- key: number,
- q: number,
- capacity: number
+    table: number[],
+    key: number,
+    q: number,
+    capacity: number
 ) {
- let hashValue: number = key % capacity;
- let loadFactor: number = capacity / 2;
- let hashFunction: number = key % capacity;
- let secondFunction: number = q - (key % q);
+    if (!table.includes(null)) {
+        return table;
+    }
+    let hashValue: number = key % capacity;
+    let loadFactor: number = capacity / 2;
+    let hashFunction: number = key % capacity;
+    let secondFunction: number = q - (key % q);
 
- if (table[hashValue] === null) {
-  table[hashValue] = key;
- } else {
-  let j: number = 1;
-  while (table[hashValue] !== null && j < loadFactor) {
-   hashValue = (hashFunction + j * secondFunction) % capacity;
-   j++;
-  }
-  if (table[hashValue] === null) {
-   table[hashValue] = key;
-  }
- }
- return table;
+    if (table[hashValue] === null) {
+        table[hashValue] = key;
+    } else {
+        let j: number = 1;
+        while (table[hashValue] !== null && j < loadFactor) {
+            hashValue = (hashFunction + j * secondFunction) % capacity;
+            j++;
+        }
+        if (table[hashValue] === null) {
+            table[hashValue] = key;
+        }
+    }
+    return table;
 }
 
 function removeDoubleHashing(
- table: number[],
- key: number,
- q: number,
- capacity: number
+    table: number[],
+    key: number,
 ) {
- let hashValue: number = key % capacity;
- let loadFactor: number = capacity / 2;
- let hashFunction: number = key % capacity;
- let secondFunction: number = q - (key % q);
-
- if (table[hashValue] === key) {
-  table[hashValue] = null;
- } else {
-  let j: number = 1;
-  while (table[hashValue] !== key && j < loadFactor) {
-   hashValue = (hashFunction + j * secondFunction) % capacity;
-   j++;
-  }
-  if (table[hashValue] === key) {
-   table[hashValue] = -2;
-  }
- }
- return table;
+    let index = table.indexOf(key);
+    if (index !== -1) {
+        table.splice(index, 1, null);
+    }
+    return table;
 }
 
 export { insertDoubleHashing, removeDoubleHashing };
