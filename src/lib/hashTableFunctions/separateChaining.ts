3adc8d0921c117f2c1d9d44d9e833687c5e0f0a4
aypuri <ayushmaan.puri18@gmail.com>
2024-05-09 16:14:57 -0700

    implemented remove and insert functions

--- src/lib/hashTableFunctions/separateChaining.ts
+++ src/lib/hashTableFunctions/separateChaining.ts
@@ -4,6 +4,11 @@ function insertSeparateChaining(
  capacity: number
 ) {
     let index = data % capacity;
+
+    if (!table[index]) {
+        table[index] = [];
+    }
+
     table[index].push(data);
     console.log(table);
     return table;
@@ -14,6 +19,21 @@ function removeSeparateChaining(
  data: number,
  capacity: number
 ) {
+    let index = data % capacity;
+    let bucket = table[index];
+    
+    let dataIndex = bucket.indexOf(data); 
+
+    if (dataIndex !== -1) { //-1 means data not found
+
+        bucket.splice(dataIndex, 1); //remove the data from the bucket
+        console.log("Data removed:", data);
+    } else {
+        console.log("Data not found:", data);
+    }
+
+    console.log(table); //console.log is used to see the changes in the table
+
  return table;
 }
 
