f2f824595a2272c29da6ba1f880a9590cc248365
Alex Espejo <aespejo102@gmail.com>
2024-05-09 01:22:26 -0700

    create bars

--- src/routes/SortingAlgorithms/Merge.svelte
+++ src/routes/SortingAlgorithms/Merge.svelte
@@ -2,10 +2,26 @@
  import Layout from "../../layouts/Layout.svelte";
  import Controls from "../../components/custom/layout/Controls.svelte";
  import Visualize from "../../components/custom/layout/Visualize.svelte";
+
+ function generateRandomArray(capacity: number) {
+  let array = [];
+  for (let i = 0; i < capacity; i++) {
+   array.push(Math.floor(Math.random() * 100));
+  }
+  return array;
+ }
+
+ let unSortNums = generateRandomArray(20);
 </script>
 
 <Layout dataStructure="SA">
  <Controls></Controls>
 
- <Visualize></Visualize>
+ <Visualize>
+  <div class="flex space-x-0.5 w-full h-96 justify-center">
+   {#each unSortNums as num, i}
+    <div class={`w-10 bg-blue-300`} style="height: {num * 2}px"></div>
+   {/each}
+  </div>
+ </Visualize>
 </Layout>
