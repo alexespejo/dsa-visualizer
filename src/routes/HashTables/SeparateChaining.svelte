fb9fe786f9e7d8b944ee13ecd7448ed7b0633f94
Alex Espejo <aespejo102@gmail.com>
2024-05-09 23:15:12 -0700

    update separate chaining ui

--- src/routes/HashTables/SeparateChaining.svelte
+++ src/routes/HashTables/SeparateChaining.svelte
@@ -5,21 +5,38 @@
 
  import FormControl from "../../components/custom/FormControl.svelte";
  import SpecialButtons from "../../components/HashTableControls/SpecialButtons.svelte";
+
  import Label from "../../components/custom/Inputs/Label.svelte";
+ import { generateRandomArray } from "../../lib/hashTableFunctions/hashTable";
  import {
   insertSeparateChaining,
   removeSeparateChaining,
  } from "../../lib/hashTableFunctions/separateChaining";
  import LabelInput from "../../components/custom/Inputs/Label.svelte";
+ import InsertionOrderDisplay from "../../components/HashTableControls/InsertionOrderDisplay.svelte";
  let hashingArray: number[][] = [[], [], [], [], [], [], [], [], [], []];
+ let insertionOrder: number[] = [];
  let numToInsert: number;
  let numToRemove: number;
 
  function insert() {
+  insertionOrder = [...insertionOrder, numToInsert];
   hashingArray = insertSeparateChaining(hashingArray, numToInsert);
+  numToInsert = null;
  }
  function remove() {
   hashingArray = removeSeparateChaining(hashingArray, numToRemove);
+  numToRemove = null;
+ }
+ function clear() {
+  hashingArray = [[], [], [], [], [], [], [], [], [], []];
+ }
+ function randomize() {
+  clear();
+  let insertionOrder = generateRandomArray(15);
+  for (let i = 0; i < insertionOrder.length; i++) {
+   hashingArray = insertSeparateChaining(hashingArray, insertionOrder[i]);
+  }
  }
 </script>
 
@@ -28,52 +45,52 @@
   <!-- Insert Button -->
   <FormControl>
    <Label>Insert Element</Label>
-   <div class="join">
+   <form class="join" on:submit|preventDefault={insert}>
     <input
      type="number"
-     class="font-bold input input-bordered input-primary w-max-w-xs w-40 join-item"
+     class="font-bold input input-sm input-bordered input-primary w-max-w-xs w-28 join-item"
      bind:value={numToInsert}
     />
     <button
-     class="btn btn-outline btn-primary w-16 join-item w-max-w-xs"
-     on:click={() => {
-      insert();
-     }}>Insert</button
+     class="btn btn-outline btn-sm btn-primary w-16 join-item w-max-w-xs"
     >
-   </div>
+     Insert
+    </button>
+   </form>
   </FormControl>
 
   <!-- Delete Button -->
   <FormControl>
-   <Label>Delete ELement</Label>
-   <div class="join">
+   <Label>Delete Element</Label>
+   <form class="join" on:submit|preventDefault={remove}>
     <input
      type="number"
-     class="font-bold input input-secondary input-bordered w-max-w-xs w-40 join-item"
+     class="font-bold input input-sm input-secondary input-bordered w-max-w-xs w-28 join-item"
      bind:value={numToRemove}
     />
     <button
-     class="btn btn-outline btn-secondary w-16 join-item w-max-w-xs"
-     on:click={() => remove()}>Delete</button
+     class="btn btn-outline btn-sm btn-secondary w-16 join-item w-max-w-xs"
+     >Delete</button
     >
-   </div>
+   </form>
   </FormControl>
 
   <FormControl>
    <LabelInput>Misc</LabelInput>
-   <div class="join">
-    <SpecialButtons clear={() => {}} randomize={() => {}} rehash={() => {}} />
+   <div class="join space-x-0.5">
+    <SpecialButtons {clear} {randomize} rehash={() => {}} />
    </div>
   </FormControl>
  </Controls>
+
  <Visualize>
-  {hashingArray}
-  <div class="lg:w-1/2">
+  <InsertionOrderDisplay {insertionOrder} />
+  <div class="lg:w-1/2 overflow-x-auto">
    <div class="space-y-1 p-5 self-start">
     {#each hashingArray as item, i}
      <div class="flex items-center">
       <div
-       class="font-bold text-xl border-2 border-neutral-content w-10 h-10 flex items-center justify-center"
+       class="font-bold text-xl border-2 border-neutral-content w-10 h-10 flex items-center justify-center min-w-10 min-h-10"
       >
        {i}
       </div>
