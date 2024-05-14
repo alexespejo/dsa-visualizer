5662cb97af328a4d25f2ed6638cb02d96a16b598
Alex Espejo <aespejo102@gmail.com>
2024-05-13 19:29:10 -0700

    amber/sky

--- src/components/custom/Buttons/Button.svelte
+++ src/components/custom/Buttons/Button.svelte
@@ -3,6 +3,21 @@
  export let color: string = "";
 </script>
 
-<button on:click class={`btn bg-${color}`}>
+<button on:click class={`btn ${color} ${color}-outline`}>
  {label}
 </button>
+
+<style>
+ .btn-amber {
+  @apply btn text-neutral bg-amber-300 hover:bg-amber-300/90 hover:border-amber-300;
+ }
+ .btn-amber {
+  @apply text-amber-300 border-amber-300 bg-base-100 hover:bg-amber-300/90 hover:text-neutral;
+ }
+ .btn-sky {
+  @apply btn text-neutral bg-sky-300 hover:bg-sky-300/90 hover:border-sky-300;
+ }
+ .btn-sky-outline {
+  @apply btn bg-base-100 btn-outline border-sky-300 text-sky-300 hover:bg-sky-300/90 hover:border-sky-300;
+ }
+</style>
