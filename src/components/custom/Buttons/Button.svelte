5bb2d848d5bb8b9fd8ef294d296aeb26394fa074
Alex Espejo <aespejo102@gmail.com>
2024-05-13 19:31:29 -0700

    limes

--- src/components/custom/Buttons/Button.svelte
+++ src/components/custom/Buttons/Button.svelte
@@ -20,4 +20,16 @@
  .btn-sky-outline {
   @apply btn bg-base-100 btn-outline border-sky-300 text-sky-300 hover:bg-sky-300/90 hover:border-sky-300;
  }
+ .btn-purple {
+  @apply btn text-neutral bg-purple-300 hover:bg-purple-300/90 hover:border-purple-300;
+ }
+ .btn-purple-outline {
+  @apply btn bg-base-100 btn-outline border-purple-300 text-purple-300 hover:bg-purple-300/90 hover:border-purple-300;
+ }
+ .btn-lime {
+  @apply btn text-neutral bg-lime-300 hover:bg-lime-300/90 hover:border-lime-300;
+ }
+ .btn-lime-outline {
+  @apply btn bg-base-100 btn-outline border-lime-300 text-lime-300 hover:bg-lime-300/90 hover:border-lime-300;
+ }
 </style>
