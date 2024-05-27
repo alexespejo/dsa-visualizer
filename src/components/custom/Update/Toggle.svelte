76c26f197faa33c46a923b71f311cfb090e9fa05
Alex Espejo <aespejo102@gmail.com>
2024-05-27 00:38:07 -0700

    created toggle.css

---
+++ src/components/custom/Update/Toggle.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+ import "./Toggle.styles.css";
+ export let checked: boolean;
+ export let color: string = "";
+ export let classList: string = "";
+</script>
+
+<input type="checkbox" bind:checked class={`toggle ${color} ${classList}`} />
