8ea3f37127a68d78ee7fbf76cd62bdd87076a78d
Alex Espejo <aespejo102@gmail.com>
2024-05-30 11:09:02 -0700

    fixed toggle styles

--- src/routes/Playgrounds/Alex.svelte
+++ src/routes/Playgrounds/Alex.svelte
@@ -1,13 +1,16 @@
 <script>
  import { onMount } from "svelte";
+ import { alexGraph_1 } from "../../lib/graphs/pGraphs";
  import Toggle from "../../components/custom/Update/Toggle.svelte";
  import FormControl from "../../components/custom/FormControl.svelte";
  import Label from "../../components/custom/Inputs/Label.svelte";
 
- let graph = Array(15).fill(Array(30).fill(0));
+ //  let graph = Array(15).fill(Array(30).fill(0));
+ let graph = alexGraph_1;
 
  let columnMaker = false;
  let rowMaker = false;
+ let destMarker = false;
 
  function markPoint(i, j) {
   if (columnMaker) {
@@ -48,16 +51,21 @@
   <Label>Column Maker</Label>
   <Toggle color="toggle-purple" bind:checked={columnMaker} />
  </FormControl>
+
+ <FormControl>
+  <Label>Place Marker</Label>
+  <Toggle color="toggle-teal" bind:checked={destMarker} />
+ </FormControl>
 </div>
 <div
- class="flex flex-col last:border-r-2 last:border-b-2"
+ class="flex flex-col border-[1px] last:border-r-[1px] last:border-b-[1px]"
  style="height: 75vh; width: 75vw"
 >
  {#each graph as row, i}
   <div class="flex" style="height: 30% ">
    {#each row as _, j}
     <button
-     class={`text-center border-t-2 border-l-2 text-xs ${graph[i][j] === 1 ? "bg-primary" : "bg-neutral"}`}
+     class={`text-center border-t-[1px] border-l-[1px] text-xs ${graph[i][j] === 1 ? "bg-blue-300" : "bg-neutral"}`}
      style="width: 30%"
      on:click={() => markPoint(i, j)}
     >
@@ -76,110 +84,3 @@
  </div>
 {/each}
 {"]"}
-<!-- <script>
-
- import Input from "../../components/custom/Update/Input.svelte";
- import Button from "../../components/custom/Update/Button.svelte";
- import FormTrigger from "../../components/custom/layout/FormTrigger.svelte";
- import Join from "../../components/custom/layout/Join.svelte";
- import FormControl from "../../components/custom/FormControl.svelte";
- import Label from "../../components/custom/Inputs/Label.svelte";
- import HiddenLabel from "../../components/custom/Inputs/HiddenLabel.svelte";
- import Range from "../../components/custom/Update/Range.svelte";
- import Toggle from "../../components/custom/Update/Toggle.svelte";
-
- let value = "";
- let rangeValue = 25;
- let toggleValue = true;
-</script>
-
-<FormControl>
- <Label>New Colors</Label>
- <Join classList="space-x-0.5">
-  <Button color="btn-amber" label="amber" />
-  <Button color="btn-sky" label="sky" />
-  <Button color="btn-purple" label="purple" />
-  <Button color="btn-teal" label="teal" />
-  <Button color="btn-indigo" label="indigo" />
-  <Button color="btn-emerald" label="emerald" />
- </Join>
-</FormControl>
-
-<div class="flex space-x-2">
- <FormControl>
-  <Label>Amber Input</Label>
-  <Join>
-   <Input placeholder="type..." bind:value color="input-amber" />
-   <Button label="Amber" color="btn-amber-outline" />
-  </Join>
- </FormControl>
-
- <FormControl>
-  <HiddenLabel />
-  <Join>
-   <Input placeholder="hidden label..." bind:value color="input-sky" />
-   <Button label="Sky" color="btn-sky-outline" />
-  </Join>
- </FormControl>
-</div>
-
-<FormControl>
- <Label>Form Trigger {"press ENTER to trigger the function"}</Label>
- <FormTrigger onTrigger={() => alert("when you press Enter something happens")}>
-  <Input placeholder="type..." bind:value color="input-emerald" />
-  <Button label="Emerald" color="btn-emerald-outline" />
- </FormTrigger>
-</FormControl>
-
-<FormControl>
- <HiddenLabel />
- <Join classList="space-x-0.5">
-  <Button label="Indigo" color="btn-indigo-outline" />
-  <Button label="Purple" color="btn-purple-outline" />
-  <Button label="Teal" color="btn-teal-outline" />
- </Join>
-</FormControl>
-
-<FormControl>
- <Label>Range Input {rangeValue}</Label>
- <div class="space-y-2 join join-vertical">
-  <Range
-   bind:value={rangeValue}
-   step={10}
-   min={10}
-   max={90}
-   color="range-sky"
-  />
-  <Range
-   bind:value={rangeValue}
-   step={10}
-   min={10}
-   max={90}
-   color="range-emerald"
-  />
-  <Range
-   bind:value={rangeValue}
-   step={10}
-   min={10}
-   max={90}
-   color="range-amber"
-  />
- </div>
-</FormControl>
-
-<FormControl>
- <Label>Range With Tick Marks</Label>
- <Range step={10} color="range-purple" markings={true} />
-</FormControl>
-
-<FormControl>
- <Label>Toggle</Label>
- <div>
-  <Toggle bind:checked={toggleValue} color="toggle-amber" />
-  <Toggle bind:checked={toggleValue} color="toggle-sky" />
-  <Toggle bind:checked={toggleValue} color="toggle-emerald" />
-  <Toggle bind:checked={toggleValue} color="toggle-purple" />
-  <Toggle bind:checked={toggleValue} color="toggle-indigo" />
-  <Toggle bind:checked={toggleValue} color="toggle-teal" />
- </div>
-</FormControl> -->
--- src/routes/Playgrounds/Kevin.svelte
+++ src/routes/Playgrounds/Kevin.svelte
@@ -1,1 +1,106 @@
-🍆
+<script>
+ import Input from "../../components/custom/Update/Input.svelte";
+ import Button from "../../components/custom/Update/Button.svelte";
+ import FormTrigger from "../../components/custom/layout/FormTrigger.svelte";
+ import Join from "../../components/custom/layout/Join.svelte";
+ import FormControl from "../../components/custom/FormControl.svelte";
+ import Label from "../../components/custom/Inputs/Label.svelte";
+ import HiddenLabel from "../../components/custom/Inputs/HiddenLabel.svelte";
+ import Range from "../../components/custom/Update/Range.svelte";
+ import Toggle from "../../components/custom/Update/Toggle.svelte";
+
+ let value = "";
+ let rangeValue = 25;
+ let toggleValue = true;
+</script>
+
+<FormControl>
+ <Label>New Colors</Label>
+ <Join classList="space-x-0.5">
+  <Button color="btn-amber" label="amber" />
+  <Button color="btn-sky" label="sky" />
+  <Button color="btn-purple" label="purple" />
+  <Button color="btn-teal" label="teal" />
+  <Button color="btn-indigo" label="indigo" />
+  <Button color="btn-emerald" label="emerald" />
+ </Join>
+</FormControl>
+
+<div class="flex space-x-2">
+ <FormControl>
+  <Label>Amber Input</Label>
+  <Join>
+   <Input placeholder="type..." bind:value color="input-amber" />
+   <Button label="Amber" color="btn-amber-outline" />
+  </Join>
+ </FormControl>
+
+ <FormControl>
+  <HiddenLabel />
+  <Join>
+   <Input placeholder="hidden label..." bind:value color="input-sky" />
+   <Button label="Sky" color="btn-sky-outline" />
+  </Join>
+ </FormControl>
+</div>
+
+<FormControl>
+ <Label>Form Trigger {"press ENTER to trigger the function"}</Label>
+ <FormTrigger onTrigger={() => alert("when you press Enter something happens")}>
+  <Input placeholder="type..." bind:value color="input-emerald" />
+  <Button label="Emerald" color="btn-emerald-outline" />
+ </FormTrigger>
+</FormControl>
+
+<FormControl>
+ <HiddenLabel />
+ <Join classList="space-x-0.5">
+  <Button label="Indigo" color="btn-indigo-outline" />
+  <Button label="Purple" color="btn-purple-outline" />
+  <Button label="Teal" color="btn-teal-outline" />
+ </Join>
+</FormControl>
+
+<FormControl>
+ <Label>Range Input {rangeValue}</Label>
+ <div class="space-y-2 join join-vertical">
+  <Range
+   bind:value={rangeValue}
+   step={10}
+   min={10}
+   max={90}
+   color="range-sky"
+  />
+  <Range
+   bind:value={rangeValue}
+   step={10}
+   min={10}
+   max={90}
+   color="range-emerald"
+  />
+  <Range
+   bind:value={rangeValue}
+   step={10}
+   min={10}
+   max={90}
+   color="range-amber"
+  />
+ </div>
+</FormControl>
+
+<FormControl>
+ <Label>Range With Tick Marks</Label>
+ <Range step={10} color="range-purple" markings={true} />
+</FormControl>
+
+<FormControl>
+ <Label>Toggle</Label>
+ <div>
+  <Toggle bind:checked={toggleValue} color="toggle-amber" />
+  <Toggle bind:checked={toggleValue} color="toggle-sky" />
+  <Toggle bind:checked={toggleValue} color="toggle-emerald" />
+  <Toggle bind:checked={toggleValue} color="toggle-purple" />
+  <Toggle bind:checked={toggleValue} color="toggle-indigo" />
+  <Toggle bind:checked={toggleValue} color="toggle-teal" />
+ </div>
+</FormControl>
