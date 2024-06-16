b8b52c1739553b05478bcd0fc06187485e5a35e7
Alex Espejo <aespejo102@gmail.com>
2024-06-15 21:16:21 -0700

    disabled links

--- src/components/Trees/TreeRoutes.svelte
+++ src/components/Trees/TreeRoutes.svelte
@@ -4,6 +4,9 @@
 
 <Link class="btn join-item" to="/trees/binary_tree">Binary Tree</Link>
 <Link class="btn join-item" to="/trees/bst">Binary Search Tree</Link>
-<Link class="btn join-item" to="/trees/avl">AVL Tree</Link>
-<!-- <Link class="btn join-item" to="/trees/rbt">Red-Black Tree</Link> -->
-<!-- <Link class="btn join-item" to="/trees/b_tree">B Tree</Link> -->
+<!-- <Link class="btn join-item" to="/trees/avl">AVL Tree</Link> -->
+<!-- <Link class="btn join-item btn-disabled" to="/trees/rbt">Red-Black Tree</Link>
+<Link class="btn join-item btn-disabled" to="/trees/b_tree">B Tree</Link> -->
+<Link class="btn join-item btn-disabled" to="">AVL Tree</Link>
+<Link class="btn join-item btn-disabled" to="">Red-Black Tree</Link>
+<Link class="btn join-item btn-disabled" to="">B Tree</Link>
